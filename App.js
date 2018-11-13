dc7e583a0c3f0ba549fbcf101e8667c73e7aa2ae
Leo Gisselberg <leo.gisselberg@digitalspark.se>
2018-11-13 22:27:19 +0100

    Local url added

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import axios from 'axios'
+import { url } from './local-url'
+ 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      msg: ''
+    }
+  }
+  componentDidMount() {
+    axios.get( url )
+      .then(res => {
+        this.setState({
+          msg: res.data.message
+        }, () => {
+          console.log(res.data.message)
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      }) 
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>
+          {this.state.msg}
+        </Text>
       </View>
     );
   }
