543b0e4a93a09a7e9ed972cd93ec5f48f1aeb881
Manjodh 'Mango' Singh <manjodheriksingh@gmail.com>
2018-11-14 20:05:33 +0100

    Added .flowconfig. App now ready for development.

--- App.js
+++ App.js
@@ -1,34 +1,26 @@
+//@flow
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import axios from 'axios'
 import { url } from './local-url'
+
+type State = {
+  
+}
  
-export default class App extends React.Component {
+export default class App extends React.Component<null, State> {
   constructor() {
     super()
-    this.state = {
-      msg: ''
-    }
   }
+
   componentDidMount() {
-    axios.get( url )
-      .then(res => {
-        this.setState({
-          msg: res.data.message
-        }, () => {
-          console.log(res.data.message)
-        })
-      })
-      .catch(err => {
-        console.log(err)
-      }) 
+    
   }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          {this.state.msg}
-        </Text>
+        
       </View>
     );
   }
